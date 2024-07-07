4e87c4c6d1455c0bcef2ebb27f4476d6b607ec8d
Dfulop98 <dobo.fulop@icloud.com>
2024-07-07 23:56:48 +0200

    Add - catppuccin theme plugin

--- init.lua
+++ init.lua
@@ -182,6 +182,14 @@ require('lazy').setup({
   --
   --  This is equivalent to:
   --    require('Comment').setup({})
+  --
+  -- theme setup
+  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
+  --    lualine integration
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+  },
   {
     'romgrk/barbar.nvim',
     dependencies = {
@@ -1069,6 +1077,78 @@ require('lazy').setup({
       --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
       --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+      -- setup lualine:
+      require('lualine').setup {
+        options = {
+          icons_enabled = true,
+          theme = 'auto',
+          component_separators = { left = '', right = '' },
+          section_separators = { left = '', right = '' },
+          disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+          },
+          ignore_focus = {},
+          always_divide_middle = true,
+          globalstatus = false,
+          refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+          },
+        },
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { 'filename' },
+          lualine_x = { 'encoding', 'fileformat', 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' },
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { 'filename' },
+          lualine_x = { 'location' },
+          lualine_y = {},
+          lualine_z = {},
+        },
+        tabline = {},
+        winbar = {},
+        inactive_winbar = {},
+        extensions = {},
+      }
+      -- theme setting:
+      require('catppuccin').setup {
+        highlight_overrides = {
+          all = function(colors)
+            return {
+              NvimTreeNormal = { fg = colors.none },
+              CmpBorder = { fg = '#3e4145' },
+            }
+          end,
+          latte = function(latte)
+            return {
+              Normal = { fg = latte.base },
+            }
+          end,
+          frappe = function(frappe)
+            return {
+              ['@comment'] = { fg = frappe.surface2, style = { 'italic' } },
+            }
+          end,
+          macchiato = function(macchiato)
+            return {
+              LineNr = { fg = macchiato.overlay1 },
+            }
+          end,
+          mocha = function(mocha)
+            return {
+              Comment = { fg = mocha.flamingo },
+            }
+          end,
+        },
+      }
     end,
   },
 
