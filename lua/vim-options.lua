1747c9530c91a337f407c0f23b47ec0672e21c4c
Dfulop98 <dobo.fulop@icloud.com>
2024-07-16 22:36:51 +0200

    Add: line move keymaps

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,9 +2,16 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-
--- map <leader> key
 vim.g.mapleader = " "
+
+-- Mozgasd a sort feljebb normál módban
+vim.api.nvim_set_keymap('n', '<A-j>', ':m .+1<CR>==', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<A-k>', ':m .-2<CR>==', { noremap = true, silent = true })
+
+-- Mozgasd a kijelölt blokkokat feljebb vagy lejjebb vizuális módban
+vim.api.nvim_set_keymap('v', '<A-j>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<A-k>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+
 
 local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
